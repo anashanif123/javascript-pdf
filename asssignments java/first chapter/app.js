039108da90673029fd713d46405b1394117f4e90
anashanif123 <154446804+anashanif123@users.noreply.github.com>
2024-03-02 21:42:53 +0500

    second commit

---
+++ asssignments java/first chapter/app.js
@@ -0,0 +1,29 @@
+// CHAPTER 1
+// ques no 1)
+alert("hello");
+
+// // ques no 2)
+alert("Error!Please enter a valid password.");                
+
+// // ques no 3)
+alert("welcome to js land... \n happy coding!");
+
+// // ques no 4)
+alert("welcome to js land...");
+
+alert("happy coding! \n prevent ");
+
+// // ques no 5)
+alert("helllo... i can run js through my web browser's console");
+// consloe.log("helllo... i can run js through my web browser's console");
+
+// // ques no 6)
+alert("hello my name is anas  ");
+
+// ques no 7)
+
+//  answer is (c);
+
+
+
+
---
+++ asssignments java/second chapter/app2.js
@@ -0,0 +1,41 @@
+// CHAPTER 2
+// ques no 1)
+var username = 'anas';
+console.log(username);
+// ques no 2)
+
+var myName = 'anas hanif';
+console.log(myName);
+
+// ques no 3)
+var message = "hello world";
+alert(message);
+
+// // ques no 4)
+var studentbiodata = 'anas';
+alert(studentbiodata);
+studentbiodata = '15years';
+alert(studentbiodata);
+studentbiodata = 'certified mobile application development';
+alert(studentbiodata);
+
+// // ques no 5)
+var food = 'pizza \n pizz \n piz \n pi \n p';
+alert(food);
+
+// // ques no 6)
+var email ='my email address is ' + ' "anashanif@gmail.com"';
+alert(email);
+
+// ques no 7)
+var book ="I am trying to learn frm the book A smarter way to learn javascript";
+alert(book)
+  
+// // ques no 8)
+var print ='yah i can write html through usinh javascript'
+document.write(print)
+
+
+// // ques no 9)
+var line = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(line)
